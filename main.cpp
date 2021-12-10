3c2ac4a1e111647ff0bfc852068ffb38fedc8f0a
PietroMilitello <91501751+PietroMilitello@users.noreply.github.com>
2021-12-10 20:27:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
 
-int main(){
-  return 0;
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+int main() {
+    int pos;
+    char n[10][20], f[1][20];
+    for (int i=0; i<10; i++){
+        cin >> n[i];
+    }
+    cin >> f[0];
+
+    if (ricerca(n, f, pos)){
+        cout << pos << endl;
+    }else{
+        cout << "non presente" << endl;
+    }
+    return 0;
 }
